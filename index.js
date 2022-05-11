cfe708afba360a1bf111e26adec18a5d4b6541ac
Oscar Escobar <qomc16@gmail.com>
2022-05-11 01:18:16 -0600

    Prueba 2

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ console.log("MR Robot");
 
 const app = require("express")();
 const serverHttp = require("http").Server(app);
-const io = require("socket.io")(serverHttp);
+const io = require("socket.io")(serverHttp, {
+  cors: {
+    origin: "http://127.0.0.1:4200",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
 const myMessages = [];
 
@@ -10,10 +17,10 @@ io.on("connection", function (socket) {
   socket.on("send-message", function (data) {
     myMessages.push(data);
     socket.emit("text-event", myMessages);
-    socket.brodcast.emit("text-event", myMessages);
+    
   });
 });
 
-serverHttp.listen(3000, () =>{
-    console.log(`server running on port  ${3000}`);
-})
+serverHttp.listen(3000, () => {
+  console.log(`server running on port  ${3000}`);
+});
