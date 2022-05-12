a17b3503974871e74e5ac8430bbf8db9281837cb
Oscar Escobar <qomc16@gmail.com>
2022-05-11 19:57:22 -0600

    Cambios importantes en el Server

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ const app = require("express")();
 const serverHttp = require("http").Server(app);
 const io = require("socket.io")(serverHttp, {
   cors: {
-    origin: "http://127.0.0.1:4200",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true,
-  },
-});
+    origin: "*"
+  }
 
-const myMessages = [];
+}
+);
 
+// en esta constante vamos a guardar todos nuestros mensajes
+const myMessages = []
+//recepcion de conexiones o mensajes
 io.on("connection", function (socket) {
-  socket.on("send-message", function (data) {
-    myMessages.push(data);
-    socket.emit("text-event", myMessages);
-    
+  socket.on('send-message', function (data) {
+    myMessages.push(data); // guarda el mensaje a la variable mymessages
+
+    //emitir el texto que se ingreso.
+    socket.emit('text-event', myMessages)
+
+    //Se envia broadcast a todos los usuarios
+    socket.broadcast.emit('text-event', myMessages)
+
   });
 });
 
+//levantar servidor http
 serverHttp.listen(3000, () => {
-  console.log(`server running on port  ${3000}`);
-});
+  console.log(`server running  port  ${3000}`);
+})
