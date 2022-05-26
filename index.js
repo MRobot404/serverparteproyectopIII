695634e82de379f455e37d104ab9ac5f06a1c297
Oscar Escobar <qomc16@gmail.com>
2022-05-26 00:54:47 -0600

    Envio de datos de usuario

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-console.log("MR Robot");
+console.log("Grupo 3");
 
 const app = require("express")();
 const serverHttp = require("http").Server(app);
 const io = require("socket.io")(serverHttp, {
   cors: {
-    origin: "*"
-  }
-
-}
-);
+    origin: "*",
+  },
+});
 
 // en esta constante vamos a guardar todos nuestros mensajes
-const myMessages = []
+const myMessages = [];
+
 //recepcion de conexiones o mensajes
 io.on("connection", function (socket) {
-  socket.on('send-message', function (data) {
-    myMessages.push(data); // guarda el mensaje a la variable mymessages
+  socket.on('conectado', function (id){
+    console.log(id)
+  })
+  socket.on("send-message", function (data) {
+    myMessages.push(data); 
+    console.log(data)// guarda el mensaje a la variable mymessages
 
     //emitir el texto que se ingreso.
-    socket.emit('text-event', myMessages)
+    socket.emit("text-event", myMessages);
 
     //Se envia broadcast a todos los usuarios
-    socket.broadcast.emit('text-event', myMessages)
-
+    io.emit("text-event", myMessages);
   });
+
 });
 
 //levantar servidor http
 serverHttp.listen(3000, () => {
   console.log(`server running  port  ${3000}`);
-})
+});
