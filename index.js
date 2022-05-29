2b603ce00ca05bbbcbd92782cd3865d6ab3b664d
Oscar Escobar <qomc16@gmail.com>
2022-05-28 22:23:39 -0600

    Usuarios conectados

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ const io = require("socket.io")(serverHttp, {
 
 // en esta constante vamos a guardar todos nuestros mensajes
 const myMessages = [];
+const myUsers=[];
 
 //recepcion de conexiones o mensajes
 io.on("connection", function (socket) {
-  socket.on('conectado', function (id){
-    console.log(id)
-  })
+  socket.on("conectado", function (data) {
+    myUsers.push(data); 
+    console.log(data)// guarda el dato en la variable myUsers
+    console.log(socket.id)
+    //emitir el texto que se ingreso.
+    socket.emit("usuarios", myUsers);
+console.log(myUsers)
+    //Se envia broadcast a todos los usuarios
+    io.emit("usuarios", myUsers);
+  });
   socket.on("send-message", function (data) {
     myMessages.push(data); 
     console.log(data)// guarda el mensaje a la variable mymessages
@@ -27,7 +35,12 @@ io.on("connection", function (socket) {
     io.emit("text-event", myMessages);
   });
 
+  socket.on("disconnect",function  (id) {
+      console.log(id);
+    })
+
 });
+
 
 //levantar servidor http
 serverHttp.listen(3000, () => {
